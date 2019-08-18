b6bc7a7e246a9510995d0609a53002a2f589dcac
심수정 <ssj591@hanyang.ac.kr>
2019-08-18 14:30:22 +0900

    add response about hi, hello

--- skills/hello_eng.py
+++ skills/hello_eng.py
@@ -3,7 +3,6 @@ from linebot.models import TextSendMessage
 from skills import add_skill
 
 
-@add_skill(r'hello|hi')
+@add_skill(r'hello'|r'hi')
 def get_location(message):
     return TextSendMessage(text='hello')
-
